9786896ad530be643548d9d914f54bb5695ddeab
tkmark <s6ksfv6f27@privaterelay.appleid.com>
2026-01-05 01:10:57 -0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Pulseio",
+  description: "Pulseio institucional",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
